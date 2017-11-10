22d89ec9c5f523f0e3c4f4f331bf1bedacbdb781
zhzzyue <zhzzyue@gmail.com>
2017-11-09 23:08:37 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/zhzzyue/pen/xPVjjg.
+
+ I did not build this from scratch, I followed the walkthrough here with minor modifications: http://thecodeplayer.com/walkthrough/matrix-rain-animation-html5-canvas-javascript. It was a totally worthwhile exercise. 
---
+++ js/index.js
@@ -0,0 +1,47 @@
+/************************************* 
+Code is based on the walkthough here: http://thecodeplayer.com/walkthrough/matrix-rain-animation-html5-canvas-javascript 
+**************************************/
+var cnvs = document.getElementById('textflow');
+var cntxt = cnvs.getContext('2d');
+
+var chars = '123456789�☐�☐�☐��☐�☐�☐��☐��☐��☐�☐�???☐�☐�☐�☐��☐�☐�☐��☐��☐��☐�☐�???☐�udj.Zf#$$#$#%^***+++==VE@@@.datatempzxyYXcache;>'; // om
+chars = chars.split(''); // make array
+var font_size = 14;
+
+// see: https://blog.codepen.io/2013/07/29/full-screen-canvas/
+function resizeCanvas() {
+  cnvs.width = window.innerWidth;
+    cnvs.height = window.innerHeight + 3000;
+
+};
+window.onresize = resizeCanvas();
+resizeCanvas();
+
+
+var columns = cnvs.width/font_size;
+var drops = [];
+for(var x=0;x<columns;x++){
+  drops[x]=1;
+}
+
+function draw(){
+  
+  cntxt.fillStyle = 'rgba(0,0,0,0.01)';
+  cntxt.fillRect(0,0,cnvs.width,cnvs.height);
+  cntxt.fillStyle = '#FFF';
+  cntxt.font = font_size + 'px helvetica';
+  
+  for(var i=0;i<drops.length;i++){
+    var txt = chars[Math.floor(Math.random()*chars.length)];
+    if(Math.random()>0.8){
+      cntxt.fillText(txt, drops[i]*font_size, i*font_size);
+    }
+    
+    if(drops[i]*font_size>cnvs.width&&Math.random()>0.975){
+      drops[i] = 0; // back to the top!   
+    }
+    drops[i]++;
+  }
+}
+
+setInterval(draw, 10);
